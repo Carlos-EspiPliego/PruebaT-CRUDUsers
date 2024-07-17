3947b1b487dd47704f0c60de5b58dbc5c5177eea
Carlos Espinoza <richcar226@gmail.com>
2024-07-17 10:06:45 -0600

    Build de la aplicación y corrección de rutas

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.scss'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/PruebaT-CRUDUsers">
         <UsersApp />
       </BrowserRouter>
     </Provider>
